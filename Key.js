8e1f017e4fc66d25028ea003bec302f6aa88ce1e
Shaunak Kishore <skishore@mit.edu>
2013-01-07 16:25:12 -0800

    Add key handling code

---
+++ Key.js
@@ -0,0 +1,27 @@
+var Key = {};
+
+Key.NUMKEYS = 7;
+
+Key.UP = 0;
+Key.RIGHT = 1;
+Key.DOWN = 2;
+Key.LEFT = 3;
+Key.DROP = 4;
+Key.HOLD = 5;
+Key.PAUSE = 6;
+
+Key.doesKeyRepeat = [false, true, true, true, false, false, false];
+
+Key.translateKeyCode = function(keyCode) {
+  switch (keyCode) {
+    case 38: return Key.UP;
+    case 39: return Key.RIGHT;
+    case 40: return Key.DOWN;
+    case 37: return Key.LEFT;
+    case 32: return Key.DROP;
+    case 16: return Key.HOLD;
+    case 13: return Key.PAUSE;
+    case 80: return Key.PAUSE;
+    default: return -1;
+  }
+}
---
+++ KeyRepeater.js
@@ -0,0 +1,72 @@
+KeyRepeater = function(pause, repeat, target) {
+  this.pause = pause;
+  this.repeat = repeat;
+
+  this.isKeyDown = [];
+  this.keyFireFrames = [];
+  for (var i = 0; i < Key.NUMKEYS; i++) {
+    this.isKeyDown.push(false);
+    this.keyFireFrames.push(-1);
+  }
+
+  // keys is accessed from outside this class.
+  this.keys = [];
+
+  target.attr('tabIndex', 1);
+  target.focus();
+  target.keydown(this.keydown_handler());
+  target.keyup(this.keyup_handler());
+}
+
+KeyRepeater.prototype.keyCode = function(e) {
+  e = e || window.event;
+  e.bubbles = false;
+  return e.keyCode;
+};
+
+KeyRepeater.prototype.keydown_handler = function() {
+  var repeater = this;
+  return function(e) {
+    var key = Key.translateKeyCode(repeater.keyCode(e));
+    if (key >= 0) {
+      repeater.isKeyDown[key] = true;
+    }
+    return false;
+  };
+};
+
+KeyRepeater.prototype.keyup_handler = function() {
+  var repeater = this;
+  return function(e) {
+    var key = Key.translateKeyCode(repeater.keyCode(e));
+    if (key >= 0) {
+      repeater.isKeyDown[key] = false;
+      if (repeater.keyFireFrames[key] < 0) {
+        repeater.keys.push(key);
+      }
+      repeater.keyFireFrames[key] = -1;
+    }
+    return false;
+  };
+};
+
+KeyRepeater.prototype.query = function(e) {
+  for (var key = 0; key < Key.NUMKEYS; key++) {
+    if (this.isKeyDown[key]) {
+      if (this.keyFireFrames[key] < 0) {
+        this.keys.push(key);
+        this.keyFireFrames[key] = this.pause;
+      } else if (this.keyFireFrames[key] == 0) {
+        if (Key.doesKeyRepeat[key]) {
+          this.keys.push(key);
+        }
+        this.keyFireFrames[key] = this.repeat;
+      } else {
+        this.keyFireFrames[key]--;
+      }
+    }
+  }
+  var result = this.keys.slice();
+  this.keys.length = 0;
+  return result;
+};
