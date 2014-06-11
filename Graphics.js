d5f9d80a4421200d91ed07e6c5b76dc6699bbb82
Shaunak Kishore <skishore@mit.edu>
2014-06-11 01:38:38 -0400

    Switch to HTML graphics backend

--- Graphics.js
+++ Graphics.js
@@ -8,60 +8,52 @@ var Graphics = function(target) {
   this.width = Constants.COLS*this.squareWidth + this.sideboard + 2*this.border;
   this.height = Constants.VISIBLEROWS*this.squareWidth + 2*this.border;
 
-  assert(this.width == target.width(), 'Error: width mismatch');
-  assert(this.height == target.height(), 'Error: height mismatch');
-  this.g = target[0].getContext('2d');
-  this.g.lineWidth = 0.5;
+  this.elements = this.build(target);
+  assert(this.width == target.outerWidth(), 'Error: width mismatch');
+  assert(this.height == target.outerHeight(), 'Error: height mismatch');
 };
 
-Graphics.prototype.fillColor = function(color) {
-  this.g.setFillColor(color);
-}
+Graphics.prototype.build = function(target) {
+  var result = {target: target}
+  result.target.css('padding', Math.floor(this.border/2) - 1);
 
-Graphics.prototype.lineColor = function(color) {
-  this.g.setStrokeColor(color);
-}
+  var border = $('<div>').addClass('ntris-border')
+  border.css('padding', Math.ceil(this.border/2) - 1);
+  result.target.append(border);
 
-Graphics.prototype.drawLine = function(x1, y1, x2, y2) {
-  assert(x1 == x2 || y1 == y2 || Math.abs(x2 - x1) == Math.abs(y2 - y1),
-      'This function only supports axis-aligned and 45-degree lines');
-  x1 += (x1 > x2);
-  x2 += (x1 < x2);
-  y1 += (y1 > y2);
-  y2 += (y1 < y2);
-  this.g.beginPath();
-  this.g.moveTo(x1 + 0.5, y1 + 0.5);
-  this.g.lineTo(x2 + 0.5, y2 + 0.5);
-  this.g.closePath();
-  this.g.stroke();
-};
+  var board = $('<div>').addClass('ntris-board').css({
+    'height': this.squareWidth*Constants.VISIBLEROWS,
+    'width': this.squareWidth*Constants.COLS,
+  });
+  border.append(board);
 
-Graphics.prototype.drawRect = function(x, y, w, h) {
-  this.g.strokeRect(x + 0.5, y + 0.5, w - 1, h - 1)
-};
+  result.board = [];
+  var hiddenRows = Constants.ROWS - Constants.VISIBLEROWS;
+  for (var i = 0; i < Constants.VISIBLEROWS; i++) {
+    var row = [];
+    for (var j = 0; j < Constants.COLS; j++) {
+      var square = $('<div>').addClass('ntris-square').css({
+        "background-color": Color.BLACK,
+        "border-color": Color.lighten(Color.BLACK),
+        "height": this.squareWidth - 2,
+        "width": this.squareWidth - 2,
+      })
+      row.push(square);
+      board.append(square);
+    }
+    result.board.push(row);
+  }
 
-Graphics.prototype.fillRect = function(x, y, w, h) {
-  this.g.fillRect(x, y, w, h)
-};
-
-Graphics.prototype.drawLineOffset = function(x1, y1, x2, y2) {
-  this.drawLine(x1 + this.border, y1 + this.border, x2 + this.border, y2 + this.border);
-};
-
-Graphics.prototype.drawRectOffset = function(x, y, w, h) {
-  this.drawRect(x + this.border, y + this.border, w, h);
-};
-
-Graphics.prototype.fillRectOffset = function(x, y, w, h) {
-  this.fillRect(x + this.border, y + this.border, w, h);
-};
-
-Graphics.prototype.clear = function() {
-  this.fillColor(Color.BLACK);
-  this.fillRect(0, 0, this.width, this.height);
+  result.sideboard = $('<div>').addClass('ntris-sideboard').css({
+    'height': this.squareWidth*Constants.VISIBLEROWS,
+    "width": this.sideboard,
+  });
+  border.append(result.sideboard);
+  return result;
 };
 
 Graphics.prototype.drawBorder = function() {
+  return;
   this.lineColor(Color.BORDER);
   this.drawRect(this.border/2 - 1, this.border/2 - 1,
       this.width - this.border + 2, this.height - this.border + 2);
@@ -70,6 +62,7 @@ Graphics.prototype.drawBorder = function() {
 };
 
 Graphics.prototype.drawGrid = function() {
+  return;
   var min_row = Constants.ROWS - Constants.VISIBLEROWS;
   for (var i = min_row; i < Constants.ROWS; i++) {
     for (var j = 0; j < Constants.COLS; j++) {
@@ -86,16 +79,12 @@ Graphics.prototype.drawBoardSquare = function(i, j, color) {
   assert(i >= 0 && i < Constants.ROWS && j >= 0 && j < Constants.COLS,
       'Invalid board square: (' + i + ', ' + j + ')');
   i -= (Constants.ROWS - Constants.VISIBLEROWS);
-  if (i < 0) {
-    return;
+  if (i >= 0) {
+    this.elements.board[i][j].css({
+      'background-color': color,
+      'border-color': Color.lighten(color),
+    });
   }
-
-  this.lineColor(Color.lighten(color));
-  this.fillColor(color);
-  this.drawRectOffset(j*this.squareWidth, i*this.squareWidth,
-      this.squareWidth, this.squareWidth);
-  this.fillRectOffset(j*this.squareWidth + 1, i*this.squareWidth + 1,
-      this.squareWidth - 2, this.squareWidth - 2);
 };
 
 Graphics.prototype.drawBlock = function(block) {
@@ -112,12 +101,6 @@ Graphics.prototype.eraseBlock = function(block) {
   }
   console.log(this.g.strokeStyle);
 }
-
-Graphics.prototype.test = function() {
-  this.clear();
-  this.drawBorder();
-  this.drawGrid();
-};
 
 return Graphics;
 })();
