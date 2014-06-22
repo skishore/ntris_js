1775572f2dfdcce916b9d9d9189dc63ab21fba86
Shaunak Kishore <kshaunak@gmail.com>
2014-06-21 22:57:29 -0400

    Get add-key working

--- Options.js
+++ Options.js
@@ -2,41 +2,87 @@ var Options = function() {
 "use strict";
 
 var Options = function(target) {
-  this.element = this.build(target);
+  this.keyCodeMap = $.extend({}, Key.keyCodeMap);
+  this.elements = this.build(target);
 }
 
 Options.prototype.build = function(target) {
-  var result = {}
+  var result = {target: target};
 
   target.append($('<h4>').text('Key bindings'));
+  target.attr('tabindex', 2);
 
   var form = $('<form>').addClass('form-horizontal');
   result.actions = [];
   for (var i = 0; i < Action.NUMACTIONS; i++) {
-    var element = this.buildActionElement(i);
+    var element = this.buildAction(i);
     form.append(element);
     result.actions.push(element);
   }
   target.append(form);
+
+  return result;
 }
 
-Options.prototype.buildActionElement = function(action) {
+Options.prototype.buildAction = function(action) {
+  var that = this;
+
   var result = $('<div>').addClass('form-group');
   var label = $('<label>')
-    .addClass('col-sm-4 control-label')
+    .addClass('col-sm-6 control-label')
     .text(Action.labels[action] + ':');
-  var keys = $('<div>').addClass('col-sm-8 ntris-options-keys');
-  for (var key in Key.keyCodeMap) {
+  var keys = $('<div>').addClass('col-sm-6 ntris-options-keys');
+  var button = $('<a>').addClass('btn btn-primary btn-sm').text('+')
+  button.click(function(e) { that.waitForKey(e, button); });
+  keys.append(button);
+  for (var key in this.keyCodeMap) {
     if (Key.keyToAction(key) == action) {
-      keys.append($('<a>').addClass('btn btn-default btn-sm')
-        .text(Key.keyNames[key] || 'Keycode ' + key)
-        .data('key', key)
-        .append($('<span>').addClass('ntris-options-close').html('&times;')));
+      keys.append(this.buildKey(key));
     }
   }
-  var button = $('<a>').addClass('btn btn-primary btn-sm').text('+');
-  result.append(label, keys.append(button));
+  result.append(label, keys);
   return result;
+}
+
+Options.prototype.buildKey = function(key) {
+  return $('<a>').addClass('btn btn-default btn-sm')
+    .text(Key.keyNames[key] || 'Keycode ' + key)
+    .data('key', key)
+    .append($('<span>').addClass('ntris-options-close').html('&times;'));
+}
+
+Options.prototype.signalReady = function(button) {
+  button.removeClass('btn-info').addClass('btn-default').text('+');
+  this.waitingButton = undefined;
+  this.elements.target.unbind('keydown');
+}
+
+Options.prototype.signalWait = function(button) {
+  var that = this;
+  button.removeClass('btn-default').addClass('btn-info').text('Press a key...');
+  this.waitingButton = button;
+  this.elements.target.keydown(function(e) { that.getKey(e, button); });
+}
+
+Options.prototype.waitForKey = function(e, button) {
+  var repeat = button == this.waitingButton;
+  if (this.waitingButton) {
+    this.signalReady(this.waitingButton);
+  }
+  if (!repeat) {
+    this.signalWait(button);
+  }
+}
+
+Options.prototype.getKey = function(e, button) {
+  this.signalReady(button);
+  button.parent().append(this.buildKey(this.keyCode(e)));
+}
+
+Options.prototype.keyCode = function(e) {
+  e = e || window.event;
+  e.bubbles = false;
+  return e.keyCode;
 }
 
 return Options;
